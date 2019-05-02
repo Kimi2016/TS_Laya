6f41d9d6e115f3556d11fd459ca0584f77fde008
Kimi2016 <Heroan2010@126.com>
2019-05-02 09:16:30 +0800

    修改index.html

--- TS_MyLaya/src/script/MainUI.ts
+++ TS_MyLaya/src/script/MainUI.ts
@@ -3,6 +3,7 @@ import NetworkManager from "../Framework/Network/NetworkManager";
 import ClientManager from "../Framework/Network/ClientManager";
 import NetEventDispatcher from "../Framework/Event/NetEventDispatcher";
 import NetPacket from "../Framework/Network/NetPacket";
+import PBName from "../Framework/Network/PBName";
 
 
 //主界面
@@ -13,7 +14,7 @@ export default class MainUI extends ui.MainSceneUI {
     onEnable(): void {
         console.log("MainUI.onEnable")
 
-        console.log(PBID.GM_VERSION_RETURN.toString());
+        console.log(PBID.GM_VERSION_RETURN);
         
         NetEventDispatcher.getInstance().RegisterMessage(PBID.GM_VERSION_RETURN, this.GM_VerifyVersionReturn)//
     }
@@ -33,7 +34,17 @@ export default class MainUI extends ui.MainSceneUI {
     onAwake(): void {
 
         console.log("Precision safe." + (Math.pow(2, 53) - 1));
-        
+
+        //var msg = {
+        //    version: "1.5.4",				//客户端版本号
+        //    platform: 9007199254740991,             ///平台
+        //    istest: 0,///    0、正常，1、测试，不需要验证版本
+        //}
+        //var mapName = PBName.getMap()
+        //var root = Laya.Browser.window["PBMessage"];
+        //var buffer: any = root[mapName[210]].encode(msg).finish();
+        //console.log(buffer);
+         
         ClientManager.getInstance().createClient(0, "ws://192.168.2.126:50000");
 
         //定时执行一次(间隔时间)
